893ee4ace8a18ba3ee891615b92fa1e396baa2f2
SClovesgtx <clovesgtx@gmail.com>
2021-03-07 12:01:33 -0300

    settings for svm model

--- models/polySVM/classifier.py
+++ models/polySVM/classifier.py
@@ -6,10 +6,21 @@ import os
 
 local_path = os.path.dirname(os.path.abspath(__file__))
 
-def train_svm_model(X_train, y_train, X_test, y_test, workspace_id):
+def train_svm_model(X_train, y_train, X_test, y_test, workspace_id, settings=None):
     y_train = np.array([np.argmax(onehot) for onehot in y_train.tolist()])
     y_test = np.array([np.argmax(onehot) for onehot in y_test.tolist()])
-    model = svm.SVC(kernel='poly', degree=1, coef0=0.0, random_state=42, probability=True) # ‘auto’ que usa 1 / n_features
+    if settings != None:
+        model = svm.SVC(    
+                    kernel='poly', 
+                    probability=True,
+                    **settings) 
+    else:
+        model = svm.SVC(    
+                    kernel='poly', 
+                    probability=True,
+                    degree=1, 
+                    coef0=0.0, 
+                    random_state=42) 
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
