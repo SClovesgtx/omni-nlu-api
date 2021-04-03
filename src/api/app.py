ae1e3f3a72339f4f9a664283a95e3fcf8ec36069
SClovesgtx <clovesgtx@gmail.com>
2021-04-03 12:04:37 -0300

    fix: style PEP-8

---
+++ src/api/app.py
@@ -0,0 +1,20 @@
+# standard library imports
+import sys
+
+sys.path.append("./src/api")
+
+# third party imports
+from flask import Flask
+
+
+def create_app() -> Flask:
+    """Create a app flask instance."""
+
+    flask_app = Flask("omni_nlu_api")
+
+    # local imports
+    from endpoints.models_settings.controller import models_settings_app
+
+    flask_app.register_blueprint(models_settings_app)
+
+    return flask_app
