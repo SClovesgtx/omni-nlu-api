36a68cd738b9f9205714f77b19be946d3dac9618
SClovesgtx <clovesgtx@gmail.com>
2021-04-13 11:47:53 -0300

    refac: data feature engineering

--- src/data_pipeline/dataflow.py
+++ src/data_pipeline/dataflow.py
@@ -9,8 +9,18 @@ from prefect import Flow, task, context
 
 # local imports
 from data_pipeline.data_sourcing import get_data
-from data_pipeline.data_preprocessing import fill_missing_examples, clean_examples
+from data_pipeline.data_preprocessing import (
+                                             fill_missing_examples, 
+                                             clean_examples,
+)
+from data_pipeline.feature_engineering import (
+                                               intents_to_onehot, 
+                                               create_corpus, 
+                                               encode_features,
+)
 from data_pipeline.data_splitting import data_splitting
+from elasticsearch_db.elasticsearch import elastic_conection
+from elasticsearch_db.elasticsearch import get_nlp_model
 
 @task
 def sourcing(workspace, es):
@@ -21,7 +31,7 @@ def sourcing(workspace, es):
             )
 
 @task
-def treat_missing_data(data):
+def treating_missing_data(data):
     
     return fill_missing_examples(data)
 
@@ -37,15 +47,37 @@ def splitting(data):
 
     return data_splitting(data)
 
+@task(nout=4)
+def encoding(train, test):
+    all_intents = set([example[0] for example in train])
+    map_onehot, intent_dictionary = intents_to_onehot(
+                                        intents_name=all_intents
+                                    )
+    all_examples = [example[0] for example in train] + \
+                   [example[0] for example in test]
+        
+    corpus = create_corpus(examples_text=all_examples)
+    
+    X_train, y_train, X_test, y_test = encode_features(
+                                            train=train, 
+                                            test=test, 
+                                            corpus=corpus, 
+                                            map_onehot=map_onehot
+                                    )
+    return X_train, y_train, X_test, y_test
+
 
 def train_data_flow(workspace, es):
 
     # Define prefect flow
-    with Flow("train_data_flow") as flow:
+    with Flow("train_dataflow") as flow:
+
         data = sourcing(workspace=workspace, es=es)
-        data = treat_missing_data(data=data)
+        data = treating_missing_data(data=data)
         data = cleansing(data=data)
         train, test = splitting(data=data)
+        X_train, y_train, X_test, y_test = encoding(train=train, 
+                                                    test=test)
 
     flow.run()
-    flow.visualize(filename="src/data_pipeline/flow_diagram/train_data_flow")
+    flow.visualize(filename="src/data_pipeline/flow_diagrams/train_dataflow")
--- src/data_pipeline/feature_engineering.py
+++ src/data_pipeline/feature_engineering.py
@@ -1,4 +1,40 @@
+# standard library imports
+from collections import namedtuple
 
+Intent = namedtuple('Intent', 'intent_name examples_text')
 
-def one_hot_encoding(train, valid, test, cols):
-    pass
+# third party imports
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def intents_to_onehot(intents_name):
+    array_intents_name = np.asarray([[intent_name] for intent_name in intents_name])
+    encoder = OneHotEncoder(sparse=False)
+    onehot_intents = encoder.fit_transform(array_intents_name) 
+    map_onehot = {
+        intent: onehot
+        for intent, onehot in zip(intents_name, onehot_intents)
+    }
+    # use to identify the intent name 
+    # in the prediction fase
+    intent_dictionary = {
+        str(np.argmax(value)): key
+        for key, value in zip(map_onehot.keys(), map_onehot.values())
+    }
+    return map_onehot, intent_dictionary
+
+def create_corpus(examples_text):
+    vectorizer = TfidfVectorizer()
+    corpus = vectorizer.fit(examples_text)
+    return corpus
+
+def encode_features(train, test, corpus, map_onehot):
+    
+    y_train = np.stack([map_onehot[example[0]] for example in train])
+    X_train = corpus.transform([example[1] for example in train]).toarray()
+    
+    y_test = np.stack([map_onehot[example[0]] for example in test])
+    X_test = corpus.transform([example[1] for example in test]).toarray()
+    
+    return X_train, y_train, X_test, y_test
